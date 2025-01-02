c94feadfa837c893aeb3fa3bfff513a022465198
bpal410 <46696804+bpal410@users.noreply.github.com>
2025-01-02 22:17:08 +0000

    Configure Projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,10 @@
 import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
+  cdkVersion: '2.173.4',
+  defaultReleaseBranch: 'master', // NOT WOKE
   name: 'cdk-jayteewashington',
   projenrcTs: true,
+  appEntrypoint: '../bin/app.ts',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
