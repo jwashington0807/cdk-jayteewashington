d185a371ab273725e87360286df0989cbb3911ef
JT Washington <jaytee.washington@gmail.com>
2025-04-24 18:48:47 -0400

    changing it up

--- infrastructure/src/lambda/emailnotification.ts
+++ infrastructure/src/lambda/emailnotification.ts
@@ -1,54 +1,61 @@
-import { SendEmailCommand, SendEmailCommandInput, SESv2Client } from '@aws-sdk/client-sesv2';
+import { SendEmailCommand, SESv2Client } from '@aws-sdk/client-sesv2';
 import { EmailTemplate } from '../ses/email-template';
 import { IEmailEvent } from '../../models/ses';
 
-let client: SESv2Client;
+const ses = new SESv2Client({ region: "us-east-1" });
 
 export const handler = async (event: IEmailEvent) => {
 
-    if(!client) {
-        client = new SESv2Client({});
+    //const date = new Date();
+    //const formattedDate = date.toLocaleDateString('en-EN', { day: 'numeric', month: 'short'});
 
-        const date = new Date();
-        const formattedDate = date.toLocaleDateString('en-EN', { day: 'numeric', month: 'short'});
-
-        const input: SendEmailCommandInput = {
-            FromEmailAddress: 'JT <noreply@jayteewashington.com>',
-            Destination: { ToAddresses: [event.reciever] },
-            EmailTags: [{ Name: 'type', Value: 'sent-confirmation' }],
-            ReplyToAddresses: ['noreply@jayteewashington.com'],
-            Content: {
-                Simple: {
-                    Subject: { Data: 'You have a new contact submission'},
-                    Body: {
-                        Html: {
-                            Charset: 'UTF-8',
-                            Data: EmailTemplate(
-                                {
-                                    id: event.id,
-                                    name: event.name,
-                                    comments: event.comments,
-                                    email: event.email,
-                                    reciever: 'jaytee.washington@gmail.com'
-                                }
-                            )
-                        },
-                        Text: { Data: 'Test Email' }
+    const command = new SendEmailCommand({
+        Destination: {
+            ToAddresses: [event.reciever],
+        },
+        FromEmailAddress: 'JT <noreply@jayteewashington.com>',
+        EmailTags: [{ Name: 'type', Value: 'sent-confirmation' }],
+        ReplyToAddresses: [ 'noreply@jayteewashington.com' ],
+        Content: {
+            Simple: {
+                Subject: { Data: 'You have a new contact submission'},
+                Body: {
+                    Html: {
+                        Charset: 'UTF-8',
+                        Data: EmailTemplate(
+                            {
+                                id: event.id,
+                                name: event.name,
+                                comments: event.comments,
+                                email: event.email,
+                                reciever: 'jaytee.washington@gmail.com'
+                            }
+                        )
                     }
                 }
             }
         }
+    });
 
-        const command = new SendEmailCommand(input);
-        await client.send(command);
+    try {
+        let response = await ses.send(command);
 
+        return {
+            statusCode: 200, 
+            headers: {
+                "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            },
+            body: JSON.stringify(response)
+        };
     }
-
-    return {
-        statusCode: 200, 
-        headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-        },
-        body: JSON.stringify("OK")
-    };
+    catch (error) {
+        
+        return {
+            statusCode: 503,
+            body: JSON.stringify(error)
+        };
+    }
+    finally {
+        // Finally 
+    }
 }
